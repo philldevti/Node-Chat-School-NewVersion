9d59c5dbee73970968eb0e2c7dc7a5f54b5c4a64
philldevti <philldevti@gmail.com>
2018-09-25 21:40:06 -0300

    Create room schema

---
+++ src/schemas/rooms.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Rooms = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    enable: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }],
+    created: {
+        type: Date,
+        required: true,
+        default: new Date()
+    }
+});
+
+module.exports = mongoose.model('Rooms', Rooms);
